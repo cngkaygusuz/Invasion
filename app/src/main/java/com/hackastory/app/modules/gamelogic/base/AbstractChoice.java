16aa5334c62d56045fb80f0717bf650f4f50ba13
Cengiz Kaygusuz <cemkyg@gmail.com>
2015-09-16 11:52:39 +0300

    ui recoloring

--- app/src/main/java/com/hackastory/app/modules/gamelogic/base/AbstractChoice.java
+++ app/src/main/java/com/hackastory/app/modules/gamelogic/base/AbstractChoice.java
@@ -1,9 +1,18 @@
 package com.hackastory.app.modules.gamelogic.base;
 
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.ActionMenuView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import com.hackastory.app.R;
 import com.hackastory.app.modules.situation.activities.SituationActivity;
 
 public abstract class AbstractChoice {
@@ -12,8 +21,8 @@ public abstract class AbstractChoice {
 
     public Button asButton(SituationActivity act, String tag) {
         Button btn = new Button(act);
-        btn.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-        btn.setText(getChoiceText());
+
+        btn.setText(getChoiceText()); btn.setTextColor(Color.parseColor("#BBBBBB"));
         btn.setOnClickListener(act);
         btn.setTag(tag);
         return btn;
